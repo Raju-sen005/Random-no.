5d192561953b0f28d5e436e1a279488391ea1c52
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2024-12-26 10:54:15 +0530

    Added the files

---
+++ r.js
@@ -0,0 +1,13 @@
+// let jokes ={
+//  0:"1",
+//  1:"2",
+//  2:"3",
+//  3:"4",
+//  4:"5",
+//  5:"6"
+// }
+let random = Math.floor(Math.random(1)*100)
+console.log(random)
+document.write(random)
+// document.write(jokes[random])
+// console.log(typeof jokes)
